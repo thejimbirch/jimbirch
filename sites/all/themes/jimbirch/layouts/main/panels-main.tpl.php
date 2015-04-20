e4b3eb7763d2847945f6c1e97b3b78bbeab30546
Jim Birch <jim@xenomedia.com>
2015-04-19 21:32:10 -0500

    Updated Layouts

--- sites/all/themes/jimbirch/layouts/main/panels-main.tpl.php
+++ sites/all/themes/jimbirch/layouts/main/panels-main.tpl.php
@@ -1,32 +1,42 @@
-<?php if (!empty($content['row-1'])): ?>
-    <div id="row-1" class="full-width">
+<?php if (!empty($content['system-content'])): ?>
+    <div id="system-content" class="full-width">
         <div class="container">
             <div class="row">
                 <div class="col-sm-8">
-                    <?php print $content['row-1']; ?>
+                    <?php print $content['system-content']; ?>
                 </div>
             </div>
         </div>
     </div>
 <?php endif; ?>
-<?php if (!empty($content['row-2'])): ?>
-    <div id="row-2" class="full-width alt">
+<?php if (!empty($content['clear-content'])): ?>
+    <div id="clear-content" class="full-width alt">
         <div class="container">
             <div class="row">
                 <div class="col-sm-8">
-                    <?php print $content['row-2']; ?>
+                    <?php print $content['clear-content']; ?>
                 </div>
+                <?php if (!empty($content['page-sidebar'])): ?>
+                    <div class="col-sm-4">
+                        <?php print $content['page-sidebar']; ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
 <?php endif; ?>
-<?php if (!empty($content['row-3'])): ?>
-    <div id="row-3" class="full-width">
+<?php if (!empty($content['main-content'])): ?>
+    <div id="main-content" class="full-width">
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 well well-lg">
-                    <?php print $content['row-3']; ?>
+                    <?php print $content['main-content']; ?>
                 </div>
+                <?php if (!empty($content['content-sidebar'])): ?>
+                    <div class="col-sm-4 well well-lg">
+                        <?php print $content['content-sidebar']; ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
