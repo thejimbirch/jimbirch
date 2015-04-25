80b2884064ff3fad973a23b0a30ddbce4fb9303d
Jim Birch <jim@xenomedia.com>
2015-04-24 23:12:37 -0500

    Node Add List Template

---
+++ sites/all/themes/jimbirch/templates/node-add-list.tpl.php
@@ -0,0 +1,10 @@
+<div id="main-content" class="full-width" data-thmr="thmr_48">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 well well-lg">
+                    <?php print render($content); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
